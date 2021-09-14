33ced264793992ee30de6e3adccd2a5a9735ea3e
Hamza BENHMANI <hamza@plot.ly>
2021-09-14 02:16:35 -0400

    Add the external getFloorPrice function

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -160,10 +160,8 @@ contract NFTMarket {
 
     /// Return the lowest listed NFT for sale in a given collection
     /// @param _nftAddress the address of the NFT contract
-    function getFloorPrice(address _nftAddress) public returns (uint256) {
-        // TODO: Implement this function!
-
-        return 1;
+    function getFloorPrice(address _nftAddress) view external returns (uint256) {
+        return listedNfts[_nftAddress].floorPrice;
     }
 
     /// Purchase the cheapest NFT in a specified collection.
